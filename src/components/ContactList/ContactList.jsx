76d02eb83b519ce653abf6bae279d63555dccc8f
vanvalera <vmarton777@gmail.com>
2023-06-03 21:52:37 +0300

    complete

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,24 +1,31 @@
 import React from 'react';
+import { RiContactsFill } from 'react-icons/ri';
+import { AiFillCloseCircle } from 'react-icons/ai';
 import shortid from 'shortid';
 import PropTypes from 'prop-types';
 import css from './ContactList.module.css';
 
 const ContactList = ({ contacts, onDeleteContact }) => {
   return (
-    <ul>
-      {contacts.map(contact => (
-        <li key={shortid.generate()} className={css.contact__list}>
-          {contact.name} : {contact.number}{' '}
-          <button
-            type="button"
-            onClick={() => onDeleteContact(contact.id)}
-            className={css.delete__contact}
-          >
-            Delete
-          </button>
-        </li>
-      ))}
-    </ul>
+    <div className={css.container}>
+      <ul className={css.contact__ul}>
+        {contacts.map(contact => (
+          <li key={shortid.generate()} className={css.contact__list}>
+            <RiContactsFill className={css.icon__contact} />
+            {contact.name} : {contact.number}{' '}
+            <button
+              type="button"
+              onClick={() => onDeleteContact(contact.id)}
+              className={css.delete__contact}
+            >
+              <span className={css.icon__close}>
+                <AiFillCloseCircle />
+              </span>
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
